9bd80847ae36eaf0aee55907bad4a7fe1d017988
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-06-11 18:52:10 +0700

    NEW: IASF_HelloSpring

---
+++ IASF_HelloSpring/IASF_HelloSpring/src/main/java/com/example/IASF_HelloSpring/IasfHelloSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.IASF_HelloSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IasfHelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IasfHelloSpringApplication.class, args);
+	}
+
+}
---
+++ IASF_HelloSpring/IASF_HelloSpring/src/test/java/com/example/IASF_HelloSpring/IasfHelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IASF_HelloSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasfHelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
