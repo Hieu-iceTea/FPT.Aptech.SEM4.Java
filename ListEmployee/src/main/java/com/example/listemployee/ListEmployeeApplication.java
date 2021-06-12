3523f6cf18531f23c5ad2e9dfa0f0f814c72ab44
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-06-12 12:07:30 +0700

    Initial: ListEmployee

---
+++ ListEmployee/src/main/java/com/example/listemployee/ListEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.listemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListEmployeeApplication.class, args);
+    }
+
+}
---
+++ ListEmployee/src/test/java/com/example/listemployee/ListEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
