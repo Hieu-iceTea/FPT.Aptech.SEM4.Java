705f52a6d36aff73afe39d946390cea62bda212a
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-07-03 10:33:12 +0700

    securing-web-mysql _ InMemory

---
+++ Spring_Guide/securing-web-mysql/src/main/java/com/example/securingwebmysql/SecuringWebMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.securingwebmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringWebMysqlApplication.class, args);
+	}
+
+}
