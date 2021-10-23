f98db69148ba221e26ca0e3ecfcd14653da66756
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-23 12:28:03 +0700

    INITIAL: hibernate_advanced_mappings

---
+++ ChadDarby_UdemyCourse/hibernate_advanced_mappings/src/main/java/com/example/hibernate_advanced_mappings/HibernateAdvancedMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernate_advanced_mappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAdvancedMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAdvancedMappingsApplication.class, args);
+	}
+
+}
