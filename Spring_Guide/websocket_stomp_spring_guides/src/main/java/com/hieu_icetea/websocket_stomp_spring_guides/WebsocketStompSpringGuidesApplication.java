8dbe43ff835cbaf58a6d8c51c49f5467cff3e276
Nguyễn Đình Hiếu <DinhHieu8896@gmail.com>
2021-10-29 10:00:31 +0700

    INITIAL: websocket_stomp_spring_guides

---
+++ Spring_Guide/websocket_stomp_spring_guides/src/main/java/com/hieu_icetea/websocket_stomp_spring_guides/WebsocketStompSpringGuidesApplication.java
@@ -0,0 +1,13 @@
+package com.hieu_icetea.websocket_stomp_spring_guides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketStompSpringGuidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketStompSpringGuidesApplication.class, args);
+	}
+
+}
---
+++ Spring_Guide/websocket_stomp_spring_guides/src/test/java/com/hieu_icetea/websocket_stomp_spring_guides/WebsocketStompSpringGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieu_icetea.websocket_stomp_spring_guides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketStompSpringGuidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
